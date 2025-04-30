081af0806aebbfcb6daee7b38f7646c53c107dcc
Cymantha <cymantha.jinto19@gmail.com>
2025-04-30 11:20:54 +0530

    initial commit

---
+++ js/data.js
@@ -0,0 +1,346 @@
+const productData = {
+    products: [
+        {
+            id: 1,
+            name: "Customized Wooden basket chocolate hamper",
+            category: "gifts",
+            price:1000 ,
+            image: "images/chocolate hamper.jpg",
+            description: "Best marriage gift for a sweet-loving couple"
+        },
+        {
+            id: 2,
+            name: "Fully customized girls baptism set",
+            category: "accessories",
+            price: 3000,
+            image: "images/abigail full blue.jpg",
+            description: "The best set to make her day the best with a white little frock and the most beautiful candle and crown"
+        },
+        {
+            id: 3,
+            name: "Blue boys candle,crown and brooch set",
+            category: "accessories",
+            price: 850,
+            image: "images/adhil name candle.jpg",
+            description: "A cute set for a little cutie's first holy communion"
+        },
+        {
+            id: 4,
+            name: "Boutonnoire set for shirt pockets",
+            category: "accessories",
+            price: 500,
+            image: "images/boutonniere purple.jpg",
+            description: "The perfect boutonnoires to match a purple colour theme"
+        },
+        {
+            id: 5,
+            name: "Bow ties for boys",
+            category: "accessories",
+            price: 100,
+            image: "images/bow tie.jpg",
+            description: "Black bow ties for boys"
+        },
+        {
+            id: 6,
+            name: "Customized purple colour bouquet",
+            category: "accessories",
+            price: 150,
+            image: "images/bridesmaid bouquet.jpg",
+            description: "The perfect bouquet for a purple colour theme"
+        },
+        {
+            id: 7,
+            name: "Customized purple bridesmaid hoop",
+            category: "accessories",
+            price: 150,
+            image: "images/bridesmaid hoop 1.jpg",
+            description: "The perfect hoop for a purple colour theme"
+        },
+        {
+            id: 8,
+            name: "Customized pink bridesmaid hoop",
+            category: "accessories",
+            price: 120,
+            image: "images/bridesmaid hoop 2.jpg",
+            description: "superb accessories to go with the Bride's pink theme"
+        },
+        {
+            id: 9,
+            name: "Silver bouquet candle and girls tiara",
+            category: "accessories",
+            price: 350 ,
+            image: "images/silver set girls.jpg",
+            description: "Silver themed communion set for your little one"
+        },
+        {
+            id: 10,
+            name: "Silver bouquet candle and boys crown",
+            category: "accessories",
+            price: 350,
+            image: "images/silver set boys.jpg",
+            description: "Silver themed communion set for your little one"
+        },
+        {
+            id: 11,
+            name: "Baby girl full baptism set",
+            category: "accessories",
+            price: 1700,
+            image: "images/girl full offwhite.jpg",
+            description: "A complete set of clothing for the cutest little girl"
+        },
+        {
+            id: 12,
+            name: "Floral hoops for flowergirls",
+            category: "accessories",
+            price:70,
+            image: "images/flower girls pink hoop.jpg",
+            description: "Cute pink floral rings for flower girls"
+        },
+        {
+            id: 13,
+            name: "Girls mitten for Holy Communion",
+            category: "accessories",
+            price: 180,
+            image: "images/gloves.jpg",
+            description: "Beautiful mittens for Holy Communion girls"
+        },
+        {
+            id: 14,
+            name: "Purple hair slide",
+            category: "accessories",
+            price: 100,
+            image: "images/hair pin purple.jpg",
+            description: "Holds the thickest of hair in place ,so that you can shine always"
+        },
+        {
+            id: 15,
+            name: "Jute brown Holy Communion set",
+            category: "accessories",
+            price: 480,
+            image: "images/jute syriac.jpg",
+            description: "The perfect jute brown natural themed Communion set for your little one"
+        },
+        {
+            id: 16,
+            name: "Twin customized pillar candle",
+            category: "accessories",
+            price: 1300,
+            image: "images/noel and nobel.jpg",
+            description: "The perfect combo of candles for twin brothers"
+        },
+        {
+            id: 17,
+            name: "Saree box and ring holder combo",
+            category: "accessories",
+            price: 1800,
+            image: "images/manthrakodi box and smallie.jpg",
+            description: "Customized green themed saree box and ring holder"
+        },
+        {
+            id: 18,
+            name: "Customized Acrylic Saree box",
+            category: "accessories",
+            price: 1200,
+            image: "images/minto acrylic box.jpg",
+            description: "A see-through saree box that showcases your beautiful saree"
+        },
+        {
+            id: 19,
+            name: "Customized green candle and tiara for girl",
+            category: "accessories",
+            price:450 ,
+            image: "images/seyona green.jpg",
+            description: "The most beautiful green Holy communion set for girls"
+        },
+        {
+            id: 20,
+            name: "Customized pink saree hoop",
+            category: "accessories",
+            price: 1200,
+            image: "images/manthrakodi hoop.jpg",
+            description: "A pink saree hoop for a pink themed marriage"
+        },
+        {
+            id: 21,
+            name: "Green themed boys baptism dress",
+            category: "accessories",
+            price: 1300,
+            image: "images/raiden dress.jpg",
+            description: "The most angelic green themed baptism dress for your little angel"
+        },
+        {
+            id: 22,
+            name: "Customized photo hoop",
+            category: "gifts",
+            price:850 ,
+            image: "images/Baptism hoop.jpg",
+            description: "Photo hoop for Baptism gifting"
+        },
+        {
+            id: 23,
+            name: "Customized wooden wall hanging",
+            category: "gifts",
+            price: 1850,
+            image: "images/christ sir.jpg",
+            description: "The perfect Wall hanging for a farewell gift"
+        },
+        {
+            id: 24,
+            name: "Lighted hoop wall hanging",
+            category: "gifts",
+            price: 850,
+            image: "images/light hoop.jpg",
+            description: "The perfect little gift for a little one"
+        },
+        {
+            id: 25,
+            name: "Girly accessory set",
+            category: "gifts",
+            price: 250,
+            image: "images/pinky girly set.jpg",
+            description: "A little pink hamper for a girl"
+        },
+        {
+            id: 26,
+            name: "Marriage gift hoop",
+            category: "gifts",
+            price: 1000,
+            image: "images/marriage hoop.jpg",
+            description: "Customized red coloured gift hoop"
+        },
+        {
+            id: 27,
+            name: "Customized wooden wall hanging",
+            category: "gifts",
+            price:2000 ,
+            image: "images/varghese father.jpg",
+            description: "Wooden wall hanging with a flower pot makes the perfect house warming gift"
+        },
+        {
+            id: 28,
+            name: "Customized 3D frame",
+            category: "gifts",
+            price: 3000,
+            image: "images/3d frame.jpg",
+            description: "A customized 3D wall hanging Photo frame for the perfect gift"
+        },
+        {
+            id: 29,
+            name: "Customized gift hamper",
+            category: "gifts",
+            price: 300,
+            image: "images/black and white hamper.jpg",
+            description: "A budget-friendly customized gift hamper for a sweet little gift"
+        },
+        {
+            id: 30,
+            name: "Navy blue Macrame Keychain with alphabets",
+            category: "return-gifts",
+            price: 35,
+            image:"images/blue macrame keychain.jpg",
+            description: "The perfect budget friendly gift for your loved ones"
+        },
+        {
+            id: 31,
+            name: "Pink Tea-light candle",
+            category: "return-gifts",
+            price: 25,
+            image: "images/pink tea candle.jpg",
+            description: "Cute little candle for marriage return gift"
+        },
+        {
+            id: 32,
+            name: "Double colour jar candles",
+            category: "return-gifts",
+            price: 60,
+            image: "images/white and blue mix jar candle.jpg",
+            description: "Trending jar candle for Christening return gifts"
+        },
+        {
+            id: 33,
+            name: "Double colour 10 bead Rosary",
+            category: "return-gifts",
+            price: 20,
+            image: "images/pink and white rosary.jpg",
+            description: "Rosary return gifts for a Christian crowd"
+        },
+        {
+            id: 34,
+            name: "Pouch keychain",
+            category: "return-gifts",
+            price: 25,
+            image: "images/pouch zip keychain.jpg",
+            description: "Budget-friendly pouch keychain"
+        },
+        {
+            id: 35,
+            name: "Thankyou card with fridge magnet and rosary",
+            category: "return-gifts",
+            price: 120,
+            image: "images/magnet card and rosary blue.jpg",
+            description: "Holy Communion return gift hamper"
+        },
+        {
+            id: 36,
+            name: "Customized lavender macrame keychain with name",
+            category: "return-gifts",
+            price:65,
+            image: "images/lavender macrame.jpg",
+            description: "The perfect trendy gift for your loved ones"
+        },
+        {
+            id: 37,
+            name: "Angel fridge magnets with Thankyou card",
+            category: "return-gifts",
+            price: 50,
+            image: "images/fridge magnet.jpg",
+            description: "Trending angel fridge magnets"
+        },
+        {
+            id: 38,
+            name: "Lavender jar candle with Thankyou candle",
+            category: "return-gifts",
+            price: 50,
+            image: "images/lavender jar candle.jpg",
+            description: "Perfect lavender jar candle for a mixed crowd"
+        },
+        {
+            id: 39,
+            name: "Rosary in a glass jar",
+            category: "return-gifts",
+            price: 30,
+            image: "images/jar and rosary.jpg",
+            description: "The perfect return gift for a Holy Communion party"
+        },
+        {
+            id: 40,
+            name:"Variety collection of threads",
+            category:"tailoring-items",
+            price: 6.00,
+            image:" images/threads.jpg",
+            description: "A vivid collection of vibrant colored threads"
+        },
+        {
+            id: 41,
+            name:"Tailoring scissors set",
+            category:"tailoring-items",
+            price: 500,
+            image:" images/scissors set.jpg",
+            description: "A scissor set consisting of everything you'll ever need for cutting through your creation"
+        },
+        {
+            id: 42,
+            name:"Lining clothes",
+            category:"tailoring-items",
+            price: 38,
+            image:" images/lining clothes.jpg",
+            description: "A vibrant collection of lining clothes"
+        }
+        
+        // Add more products as needed
+    ]
+};
+
+let userData = {
+    users: []
+};
---
+++ js/script.js
@@ -0,0 +1,238 @@
+let currentUser = null;
+let cart = [];
+
+// Initialize the page
+document.addEventListener('DOMContentLoaded', () => {
+    loadProducts();
+    setupNavigation();
+    loadCartFromSession();
+    loadUserData();
+});
+
+// Navigation
+function setupNavigation() {
+    document.querySelectorAll('.nav-link').forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            const targetId = e.target.getAttribute('href').substring(1);
+            showSection(targetId);
+        });
+    });
+}
+
+function navigateToCart() {
+	showSection("cart");
+	updateCartDisplay();
+}
+
+function showSection(sectionId) {
+    document.querySelectorAll('.section').forEach(section => {
+        section.classList.remove('active');
+    });
+    document.getElementById(sectionId).classList.add('active');
+}
+
+// Product handling
+function loadProducts(category = 'all') {
+    const productList = document.getElementById('productList');
+    productList.innerHTML = '';
+    
+    const filteredProducts = category === 'all' 
+        ? productData.products 
+        : productData.products.filter(p => p.category === category);
+
+    filteredProducts.forEach(product => {
+        const productCard = createProductCard(product);
+        productList.appendChild(productCard);
+    });
+}
+
+function createProductCard(product) {
+    const div = document.createElement('div');
+    div.className = 'col-md-4 col-sm-6 product-card';
+    div.innerHTML = `
+        <div class="card">
+            <img src="${product.image}" class="card-img-top" alt="${product.name}">
+            <div class="card-body">
+                <h5 class="card-title">${product.name}</h5>
+                <p class="card-text">${product.description}</p>
+                <p class="card-text">₹${product.price}</p>
+                <button class="btn btn-primary" onclick="addToCart(${product.id})">
+                    Add to Cart
+                </button>
+            </div>
+        </div>
+    `;
+    return div;
+}
+
+function filterProducts(category) {
+    loadProducts(category);
+}
+
+function searchProducts() {
+    const searchTerm = document.getElementById('searchInput').value.toLowerCase();
+    const filteredProducts = productData.products.filter(product => 
+        product.name.toLowerCase().includes(searchTerm) || 
+        product.description.toLowerCase().includes(searchTerm)
+    );
+    
+    const productList = document.getElementById('productList');
+    productList.innerHTML = '';
+    filteredProducts.forEach(product => {
+        const productCard = createProductCard(product);
+        productList.appendChild(productCard);
+    });
+}
+
+// Cart handling
+function addToCart(productId) {
+    const product = productData.products.find(p => p.id === productId);
+    if (product) {
+        cart.push(product);
+        updateCartCount();
+        saveCartToSession();
+        alert('Product added to cart!');
+    }
+}
+
+function updateCartCount() {
+    document.getElementById('cartCount').textContent = cart.length;
+}
+
+function updateCartDisplay() {
+    const cartItems = document.getElementById('cartItems');
+    cartItems.innerHTML = '';
+    let total = 0;
+
+    cart.forEach((item, index) => {
+        total += item.price;
+        const itemElement = document.createElement('div');
+        itemElement.className = 'card mb-3';
+        itemElement.innerHTML = `
+            <div class="card-body d-flex align-items-center">
+                <img src="${item.image}" class="cart-item-image me-3" alt="${item.name}">
+                <div class="flex-grow-1">
+                    <h5 class="card-title">${item.name}</h5>
+                    <p class="card-text">₹${item.price}</p>
+                </div>
+                <button class="btn btn-danger" onclick="removeFromCart(${index})">Remove</button>
+            </div>
+        `;
+        cartItems.appendChild(itemElement);
+    });
+
+    document.getElementById('cartTotal').textContent = total.toFixed(2);
+}
+
+function removeFromCart(index) {
+    cart.splice(index, 1);
+    updateCartCount();
+    saveCartToSession();
+    updateCartDisplay();
+}
+
+function checkout() {
+    if (!currentUser) {
+        alert('Please register before checkout');
+        showSection('register');
+        return;
+    }
+    
+    generateInvoice();
+    showSection('invoice');
+}
+
+function generateInvoice() {
+    const invoiceContent = document.getElementById('invoiceContent');
+    let total = cart.reduce((sum, item) => sum + item.price, 0);
+    
+    invoiceContent.innerHTML = `
+        <div class="row mb-4">
+            <div class="col-6">
+                <h5>Bill To:</h5>
+                <p>${currentUser.name}<br>
+                ${currentUser.address}<br>
+                ${currentUser.email}<br>
+                ${currentUser.phone}</p>
+            </div>
+            <div class="col-6 text-end">
+                <h5>Invoice Date:</h5>
+                <p>${new Date().toLocaleDateString()}</p>
+            </div>
+        </div>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Item</th>
+                    <th>Price</th>
+                </tr>
+            </thead>
+            <tbody>
+                ${cart.map(item => `
+                    <tr>
+                        <td>${item.name}</td>
+                        <td>₹${item.price}</td>
+                    </tr>
+                `).join('')}
+                <tr>
+                    <th>Total</th>
+                    <th>₹${total.toFixed(2)}</th>
+                </tr>
+            </tbody>
+        </table>
+    `;
+}
+
+// User Registration
+function registerUser(event) {
+    event.preventDefault();
+    
+    const user = {
+        name: document.getElementById('regName').value,
+        email: document.getElementById('regEmail').value,
+        address: document.getElementById('regAddress').value,
+        phone: document.getElementById('regPhone').value
+    };
+    
+    userData.users.push(user);
+    currentUser = user;
+    saveUserData();
+    
+    alert('Registration successful!');
+    showSection('products');
+}
+
+// Storage functions
+function saveCartToSession() {
+    sessionStorage.setItem('cart', JSON.stringify(cart));
+}
+
+function loadCartFromSession() {
+    const savedCart = sessionStorage.getItem('cart');
+    if (savedCart) {
+        cart = JSON.parse(savedCart);
+        updateCartCount();
+    }
+}
+
+function saveUserData() {
+    sessionStorage.setItem('userData', JSON.stringify(userData));
+    sessionStorage.setItem('currentUser', JSON.stringify(currentUser));
+}
+
+function loadUserData() {
+    const savedUserData = sessionStorage.getItem('userData');
+    const savedCurrentUser = sessionStorage.getItem('currentUser');
+    
+    if (savedUserData) {
+        userData = JSON.parse(savedUserData);
+    }
+    if (savedCurrentUser) {
+        currentUser = JSON.parse(savedCurrentUser);
+    }
+}
+
+function printInvoice() {
+    window.print();
+}
