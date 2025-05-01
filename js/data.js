f2572e489884b34967af7313e17b41e18a5c8681
Cymantha <cymantha.jinto19@gmail.com>
2025-05-02 00:08:49 +0530

    Updated prices

--- js/data.js
+++ js/data.js
@@ -4,7 +4,7 @@ const productData = {
             id: 1,
             name: "Customized Wooden basket chocolate hamper",
             category: "gifts",
-            price:1000 ,
+            price: 1000.00,
             image: "images/chocolate hamper.jpg",
             description: "Best marriage gift for a sweet-loving couple"
         },
@@ -12,7 +12,7 @@ const productData = {
             id: 2,
             name: "Fully customized girls baptism set",
             category: "accessories",
-            price: 3000,
+            price: 3000.00,
             image: "images/abigail full blue.jpg",
             description: "The best set to make her day the best with a white little frock and the most beautiful candle and crown"
         },
@@ -20,23 +20,23 @@ const productData = {
             id: 3,
             name: "Blue boys candle,crown and brooch set",
             category: "accessories",
-            price: 850,
+            price: 850.00,
             image: "images/adhil name candle.jpg",
             description: "A cute set for a little cutie's first holy communion"
         },
         {
             id: 4,
-            name: "Boutonnoire set for shirt pockets",
+            name: "Boutonniere set for shirt pockets",
             category: "accessories",
-            price: 500,
+            price: 500.00,
             image: "images/boutonniere purple.jpg",
-            description: "The perfect boutonnoires to match a purple colour theme"
+            description: "The perfect boutonnieres to match a purple colour theme"
         },
         {
             id: 5,
             name: "Bow ties for boys",
             category: "accessories",
-            price: 100,
+            price: 100.00,
             image: "images/bow tie.jpg",
             description: "Black bow ties for boys"
         },
@@ -44,7 +44,7 @@ const productData = {
             id: 6,
             name: "Customized purple colour bouquet",
             category: "accessories",
-            price: 150,
+            price: 150.00,
             image: "images/bridesmaid bouquet.jpg",
             description: "The perfect bouquet for a purple colour theme"
         },
@@ -52,7 +52,7 @@ const productData = {
             id: 7,
             name: "Customized purple bridesmaid hoop",
             category: "accessories",
-            price: 150,
+            price: 150.00,
             image: "images/bridesmaid hoop 1.jpg",
             description: "The perfect hoop for a purple colour theme"
         },
@@ -60,7 +60,7 @@ const productData = {
             id: 8,
             name: "Customized pink bridesmaid hoop",
             category: "accessories",
-            price: 120,
+            price: 120.00,
             image: "images/bridesmaid hoop 2.jpg",
             description: "superb accessories to go with the Bride's pink theme"
         },
@@ -68,7 +68,7 @@ const productData = {
             id: 9,
             name: "Silver bouquet candle and girls tiara",
             category: "accessories",
-            price: 350 ,
+            price: 350.00,
             image: "images/silver set girls.jpg",
             description: "Silver themed communion set for your little one"
         },
@@ -76,7 +76,7 @@ const productData = {
             id: 10,
             name: "Silver bouquet candle and boys crown",
             category: "accessories",
-            price: 350,
+            price: 350.00,
             image: "images/silver set boys.jpg",
             description: "Silver themed communion set for your little one"
         },
@@ -84,7 +84,7 @@ const productData = {
             id: 11,
             name: "Baby girl full baptism set",
             category: "accessories",
-            price: 1700,
+            price: 1700.00,
             image: "images/girl full offwhite.jpg",
             description: "A complete set of clothing for the cutest little girl"
         },
@@ -92,7 +92,7 @@ const productData = {
             id: 12,
             name: "Floral hoops for flowergirls",
             category: "accessories",
-            price:70,
+            price: 70.00,
             image: "images/flower girls pink hoop.jpg",
             description: "Cute pink floral rings for flower girls"
         },
@@ -100,7 +100,7 @@ const productData = {
             id: 13,
             name: "Girls mitten for Holy Communion",
             category: "accessories",
-            price: 180,
+            price: 180.00,
             image: "images/gloves.jpg",
             description: "Beautiful mittens for Holy Communion girls"
         },
@@ -108,7 +108,7 @@ const productData = {
             id: 14,
             name: "Purple hair slide",
             category: "accessories",
-            price: 100,
+            price: 100.00,
             image: "images/hair pin purple.jpg",
             description: "Holds the thickest of hair in place ,so that you can shine always"
         },
@@ -116,7 +116,7 @@ const productData = {
             id: 15,
             name: "Jute brown Holy Communion set",
             category: "accessories",
-            price: 480,
+            price: 480.00,
             image: "images/jute syriac.jpg",
             description: "The perfect jute brown natural themed Communion set for your little one"
         },
@@ -124,7 +124,7 @@ const productData = {
             id: 16,
             name: "Twin customized pillar candle",
             category: "accessories",
-            price: 1300,
+            price: 1300.00,
             image: "images/noel and nobel.jpg",
             description: "The perfect combo of candles for twin brothers"
         },
@@ -132,7 +132,7 @@ const productData = {
             id: 17,
             name: "Saree box and ring holder combo",
             category: "accessories",
-            price: 1800,
+            price: 1800.00,
             image: "images/manthrakodi box and smallie.jpg",
             description: "Customized green themed saree box and ring holder"
         },
@@ -140,7 +140,7 @@ const productData = {
             id: 18,
             name: "Customized Acrylic Saree box",
             category: "accessories",
-            price: 1200,
+            price: 1200.00,
             image: "images/minto acrylic box.jpg",
             description: "A see-through saree box that showcases your beautiful saree"
         },
@@ -148,7 +148,7 @@ const productData = {
             id: 19,
             name: "Customized green candle and tiara for girl",
             category: "accessories",
-            price:450 ,
+            price: 450.00,
             image: "images/seyona green.jpg",
             description: "The most beautiful green Holy communion set for girls"
         },
@@ -156,7 +156,7 @@ const productData = {
             id: 20,
             name: "Customized pink saree hoop",
             category: "accessories",
-            price: 1200,
+            price: 1200.00,
             image: "images/manthrakodi hoop.jpg",
             description: "A pink saree hoop for a pink themed marriage"
         },
@@ -164,7 +164,7 @@ const productData = {
             id: 21,
             name: "Green themed boys baptism dress",
             category: "accessories",
-            price: 1300,
+            price: 1300.00,
             image: "images/raiden dress.jpg",
             description: "The most angelic green themed baptism dress for your little angel"
         },
@@ -172,7 +172,7 @@ const productData = {
             id: 22,
             name: "Customized photo hoop",
             category: "gifts",
-            price:850 ,
+            price: 850.00,
             image: "images/Baptism hoop.jpg",
             description: "Photo hoop for Baptism gifting"
         },
@@ -180,7 +180,7 @@ const productData = {
             id: 23,
             name: "Customized wooden wall hanging",
             category: "gifts",
-            price: 1850,
+            price: 1850.00,
             image: "images/christ sir.jpg",
             description: "The perfect Wall hanging for a farewell gift"
         },
@@ -188,7 +188,7 @@ const productData = {
             id: 24,
             name: "Lighted hoop wall hanging",
             category: "gifts",
-            price: 850,
+            price: 850.00,
             image: "images/light hoop.jpg",
             description: "The perfect little gift for a little one"
         },
@@ -196,7 +196,7 @@ const productData = {
             id: 25,
             name: "Girly accessory set",
             category: "gifts",
-            price: 250,
+            price: 250.00,
             image: "images/pinky girly set.jpg",
             description: "A little pink hamper for a girl"
         },
@@ -204,7 +204,7 @@ const productData = {
             id: 26,
             name: "Marriage gift hoop",
             category: "gifts",
-            price: 1000,
+            price: 1000.00,
             image: "images/marriage hoop.jpg",
             description: "Customized red coloured gift hoop"
         },
@@ -212,7 +212,7 @@ const productData = {
             id: 27,
             name: "Customized wooden wall hanging",
             category: "gifts",
-            price:2000 ,
+            price: 2000.00,
             image: "images/varghese father.jpg",
             description: "Wooden wall hanging with a flower pot makes the perfect house warming gift"
         },
@@ -220,7 +220,7 @@ const productData = {
             id: 28,
             name: "Customized 3D frame",
             category: "gifts",
-            price: 3000,
+            price: 3000.00,
             image: "images/3d frame.jpg",
             description: "A customized 3D wall hanging Photo frame for the perfect gift"
         },
@@ -228,7 +228,7 @@ const productData = {
             id: 29,
             name: "Customized gift hamper",
             category: "gifts",
-            price: 300,
+            price: 300.00,
             image: "images/black and white hamper.jpg",
             description: "A budget-friendly customized gift hamper for a sweet little gift"
         },
@@ -236,7 +236,7 @@ const productData = {
             id: 30,
             name: "Navy blue Macrame Keychain with alphabets",
             category: "return-gifts",
-            price: 35,
+            price: 35.00,
             image:"images/blue macrame keychain.jpg",
             description: "The perfect budget friendly gift for your loved ones"
         },
@@ -244,7 +244,7 @@ const productData = {
             id: 31,
             name: "Pink Tea-light candle",
             category: "return-gifts",
-            price: 25,
+            price: 25.00,
             image: "images/pink tea candle.jpg",
             description: "Cute little candle for marriage return gift"
         },
@@ -252,7 +252,7 @@ const productData = {
             id: 32,
             name: "Double colour jar candles",
             category: "return-gifts",
-            price: 60,
+            price: 60.00,
             image: "images/white and blue mix jar candle.jpg",
             description: "Trending jar candle for Christening return gifts"
         },
@@ -260,7 +260,7 @@ const productData = {
             id: 33,
             name: "Double colour 10 bead Rosary",
             category: "return-gifts",
-            price: 20,
+            price: 20.00,
             image: "images/pink and white rosary.jpg",
             description: "Rosary return gifts for a Christian crowd"
         },
@@ -268,7 +268,7 @@ const productData = {
             id: 34,
             name: "Pouch keychain",
             category: "return-gifts",
-            price: 25,
+            price: 25.00,
             image: "images/pouch zip keychain.jpg",
             description: "Budget-friendly pouch keychain"
         },
@@ -276,7 +276,7 @@ const productData = {
             id: 35,
             name: "Thankyou card with fridge magnet and rosary",
             category: "return-gifts",
-            price: 120,
+            price: 120.00,
             image: "images/magnet card and rosary blue.jpg",
             description: "Holy Communion return gift hamper"
         },
@@ -284,7 +284,7 @@ const productData = {
             id: 36,
             name: "Customized lavender macrame keychain with name",
             category: "return-gifts",
-            price:65,
+            price: 65.00,
             image: "images/lavender macrame.jpg",
             description: "The perfect trendy gift for your loved ones"
         },
@@ -292,7 +292,7 @@ const productData = {
             id: 37,
             name: "Angel fridge magnets with Thankyou card",
             category: "return-gifts",
-            price: 50,
+            price: 50.00,
             image: "images/fridge magnet.jpg",
             description: "Trending angel fridge magnets"
         },
@@ -300,7 +300,7 @@ const productData = {
             id: 38,
             name: "Lavender jar candle with Thankyou candle",
             category: "return-gifts",
-            price: 50,
+            price: 50.00,
             image: "images/lavender jar candle.jpg",
             description: "Perfect lavender jar candle for a mixed crowd"
         },
@@ -308,7 +308,7 @@ const productData = {
             id: 39,
             name: "Rosary in a glass jar",
             category: "return-gifts",
-            price: 30,
+            price: 30.00,
             image: "images/jar and rosary.jpg",
             description: "The perfect return gift for a Holy Communion party"
         },
@@ -324,7 +324,7 @@ const productData = {
             id: 41,
             name:"Tailoring scissors set",
             category:"tailoring-items",
-            price: 500,
+            price: 500.00,
             image:" images/scissors set.jpg",
             description: "A scissor set consisting of everything you'll ever need for cutting through your creation"
         },
@@ -332,7 +332,7 @@ const productData = {
             id: 42,
             name:"Lining clothes",
             category:"tailoring-items",
-            price: 38,
+            price: 38.00,
             image:" images/lining clothes.jpg",
             description: "A vibrant collection of lining clothes"
         }
